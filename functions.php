874789e5d24017851aef0b2255c7b4e6d015bae2
Sal Ferrarello <sal@ironcodestudio.com>
2017-07-11 10:29:10 -0400

    Add PHP to create CMB2 title color metabox on posts
    Note: this code requires the [CMB2 plugin](https://wordpress.org/plugins/cmb2/).
    be installed and activated.
    
    Without the CMB2 plugin, the color metabox will NOT be created.

--- functions.php
+++ functions.php
@@ -15,3 +15,26 @@ function fe_wcdc_post_meta_twentysixteen_childtheme_css() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
+
+
+
+add_action( 'cmb2_admin_init', 'fe_wcdc_color_metabox' );
+
+/**
+ * Use CMB2 to create a color picker metabox for `fe_wcdc_color`.
+ *
+ * Requires: [CMB2 plugin](https://wordpress.org/plugins/cmb2/).
+ */
+function fe_wcdc_color_metabox() {
+	$cmb = new_cmb2_box( array(
+		'id'            => 'fe_wcdc_color_metabox',
+		'title'         => esc_html__( 'Color Metabox', 'wcdc' ),
+		'object_types'  => array( 'post' ), // Post type.
+	) );
+
+	$cmb->add_field( array(
+		'name'       => esc_html__( 'Title Color', 'wcdc' ),
+		'id'         => 'fe_wcdc_color',
+		'type'       => 'colorpicker',
+	) );
+}
